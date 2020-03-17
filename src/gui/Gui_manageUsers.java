56caae80c00bcd0a491d1839866106c43e6b91c5
cyoni <cyoni10@gmail.com>
2020-03-17 16:38:05 +0200

    Finished working on this

--- src/gui/Gui_manageUsers.java
+++ src/gui/Gui_manageUsers.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 import javax.swing.JComboBox;
 import utils.User_Dialog;
 
@@ -45,9 +46,8 @@ public class Gui_manageUsers extends javax.swing.JFrame {
         jButton5 = new javax.swing.JButton();
         jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        jComboBox1 = new javax.swing.JComboBox<>();
+        users_list = new javax.swing.JComboBox<>();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
 
@@ -60,11 +60,14 @@ public class Gui_manageUsers extends javax.swing.JFrame {
 
         jLabel1.setText("Grant/Revoke permissions:");
 
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        users_list.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
-        jButton1.setText("Grant Permission");
-
-        jButton2.setText("Revoke Permission");
+        jButton1.setText("Grant/Revoke Permission");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         jButton4.setText("New User");
         jButton4.addActionListener(new java.awt.event.ActionListener() {
@@ -74,6 +77,11 @@ public class Gui_manageUsers extends javax.swing.JFrame {
         });
 
         jButton6.setText("Remove User");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -81,28 +89,21 @@ public class Gui_manageUsers extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                        .addComponent(users_list, javax.swing.GroupLayout.PREFERRED_SIZE, 353, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(jButton1)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(jButton6, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                            .addComponent(jButton4, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
+                        .addGap(19, 19, 19)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(22, 22, 22)
-                                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(30, 30, 30)
-                                .addComponent(jLabel1))
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(41, 41, 41)
-                                .addComponent(jButton1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton2)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton4)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(jButton6)))
-                        .addGap(0, 0, Short.MAX_VALUE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 456, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -112,11 +113,10 @@ public class Gui_manageUsers extends javax.swing.JFrame {
                 .addGap(35, 35, 35)
                 .addComponent(jLabel1)
                 .addGap(30, 30, 30)
-                .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(users_list, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(30, 30, 30)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
-                    .addComponent(jButton2)
                     .addComponent(jButton4)
                     .addComponent(jButton6))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -124,22 +124,81 @@ public class Gui_manageUsers extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
-
-        User_Dialog.showAlert("you email is: " + User.getEmail());
-        
+    
+    private void addUser(){
+           
         String email =  User_Dialog.getInputDialog("Enter the email of the user:");
-    if (email.isEmpty()) return;
+    if (email==null) return;
+    email = email.trim();
+    boolean ans = isValid(email);
     
-         //   database.query_update("INSERT INTO permissions (owner, usr_email) VALUES ('"+ User.getEmail() +"', '"+ email +"') IF NOT EXISTS"
-           //         + " (SELECT usr_email from permissions where owner='"+ User.getEmail() +"');");
-        
-        
+    if (!ans) addUser();
+        else{
+
+
+            for (int i=0; i< users_list.getItemCount(); i++){
+                if (users_list.getItemAt(i).equals(email)){
+                    User_Dialog.showAlert(email + " already exists!");
+                    return;
+                }
+            }
+                database.query_update("INSERT INTO permissions (owner, usr_email)\n" +
+            "    VALUES ('"+ User.getEmail() +"', '"+ email +"');");
+                users_list.addItem(email + " (unauthorized)");
+        }
+    }
     
-        
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+     addUser();
     }//GEN-LAST:event_jButton4ActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        String[] arr = getStatusFromList(users_list.getSelectedItem().toString());
+        String email = arr[0];
+ 
+        String status = arr[1];
+        int changeTo = -1;
+        users_list.setEditable(true);
+        if (status.equals("authorized")) {changeTo=-1; users_list.setSelectedItem(email + " (unauthorized)"); }      
+        else if (status.equals("unauthorized")) {changeTo=1; users_list.setSelectedItem(email + " (authorized)"); }      
+        users_list.setEditable(false);
+          database.query_update("UPDATE permissions SET permission="+ changeTo +" WHERE owner='"+ User.getEmail() +"' AND usr_email='"+ email +"';");
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        String str = users_list.getSelectedItem().toString();
+        String[] arr = getStatusFromList(str);
+        String email = arr[0];
+        
+      database.query_update("DELETE from permissions WHERE owner='"+ User.getEmail() +"' AND usr_email='"+ email +"';");
+      users_list.removeItem(str);
+
+    }//GEN-LAST:event_jButton6ActionPerformed
+
+    
+    /**
+     * This method verifies whether the syntax of the email address is correct.
+     * @param email
+     * @return 
+     */
+     public static boolean isValid(String email) 
+    { 
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+ 
+                            "[a-zA-Z0-9_+&*-]+)*@" + 
+                            "(?:[a-zA-Z0-9-]+\\.)+[a-z" + 
+                            "A-Z]{2,7}$"; 
+                              
+        Pattern pat = Pattern.compile(emailRegex); 
+        if (email == null) 
+            return false; 
+        return pat.matcher(email).matches(); 
+    } 
+    
+    
+    
+    
     /**
      * @param args the command line arguments
      */
@@ -177,13 +236,12 @@ public class Gui_manageUsers extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
-    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JComboBox<String> users_list;
     // End of variables declaration//GEN-END:variables
 
     void setUser(user User) {
@@ -192,16 +250,17 @@ public class Gui_manageUsers extends javax.swing.JFrame {
     }
 
     private void loadUserList() {
-        ResultSet rs = database.query("SELECT usr_email FROM permissions where owner='"+ User.getEmail() +"';");
-        
+        users_list.removeAllItems();
+        ResultSet rs = database.query("SELECT usr_email, permission FROM permissions where owner='"+ User.getEmail() +"';");
         try {
             while (rs.next()){
                 String email = rs.getString("usr_email");
-               // list.addItem(email);
-                User_Dialog.showAlert("adding " + email);
+                int permission = rs.getInt("permission");
+               if (permission == -1) email+= " (unauthorized)";
+               else if (permission == 1) email += " (authorized)";
+               
+               users_list.addItem(email);
             }
-                            User_Dialog.showAlert("done");
-
         } catch (SQLException ex) {
             Logger.getLogger(Gui_manageUsers.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -209,6 +268,13 @@ public class Gui_manageUsers extends javax.swing.JFrame {
 
     }
 
-    
+    private String[] getStatusFromList(String str) {
+       if (str.isEmpty()) return null;
+        String what[] = str.split(" \\(");
+        String email = what[0].trim();
+        String status = what[1];
+        status = status.substring(0, status.length()-1);
+        return new String[]{email , status};
+    }
 
 }
