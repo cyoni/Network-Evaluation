56bb07affe08d6b6164748bd608c425f5c4c46c1
cyoni <cyoni10@gmail.com>
2020-04-05 12:58:53 +0300

    added notes

--- src/graph_visualization/Draw.java
+++ src/graph_visualization/Draw.java
@@ -10,6 +10,7 @@ import algorithms.ConstructGraph;
 import algorithms.Graph_Algo;
 import algorithms.line;
 import graph.Graph;
+import graph.Node;
 import graph.edge_metadata;
 import java.awt.Color;
 import java.util.ArrayList;
@@ -55,7 +56,7 @@ public class Draw {
             StdDraw.point(n.getLocation().x(), n.getLocation().y()); // draw the node(point)
             // print the key of the nodes
             StdDraw.setPenColor(StdDraw.BLACK);
-            StdDraw.text(n.getLocation().x()+1, n.getLocation().y()+1.5, n.getName() + "(" + n.getKey() + ")"); // print the id of the point
+            StdDraw.text(n.getLocation().x()+1, n.getLocation().y()+1.5, n.getName() + "(" + Math.floor(n.getLocation().x()) + "," + Math.floor( n.getLocation().y()) + ")"); // print the id of the point
           }
 
           // draw all edges
@@ -73,15 +74,53 @@ public class Draw {
                 
                   // print the edges and connection
                    StdDraw.setPenRadius(0.005);
-                   Point2D text_pos = line.getPointOnLine(new Point2D(g.getNode(current_edge.getSrc()).getLocation().x(), g.getNode(current_edge.getSrc()).getLocation().y()),
-                     new Point2D(g.getNode(current_edge.getDest()).getLocation().x(), g.getNode(current_edge.getDest()).getLocation().y()), 50);
-                     StdDraw.text(text_pos.x()-1.5, text_pos.y()+1.5, g.getEdge(src, dest).getTag()+ ""); // print text connection
+                   node_metadata Node_src = g.getNode(current_edge.getSrc());
+                   node_metadata Node_dest = g.getNode(current_edge.getDest());
+                   
+                   Point2D label_location = line.getPointOnLine(new Point2D(Node_src.getLocation().x(), Node_src.getLocation().y()),
+                     new Point2D(Node_dest.getLocation().x(), Node_dest.getLocation().y()), 50);
+                   double x1 = Node_src.getLocation().x();
+                   double y1 = Node_src.getLocation().y();
+                   double x2 = Node_dest.getLocation().x();
+                   double y2 = Node_dest.getLocation().y();
+                   
+                   double m = (y2-y1)/(x2-x1);
+                          
+                   drawConnection(Node_src, Node_dest, label_location);
                      
                      StdDraw.line(g.getNode(current_edge.getSrc()).getLocation().x(), g.getNode(current_edge.getSrc()).getLocation().y(),
                             g.getNode(current_edge.getDest()).getLocation().x(), g.getNode(current_edge.getDest()).getLocation().y());  // draw line 
-                     
+                  
+                             
 //                     StdDraw.text(n.getLocation().x()+1, n.getLocation().y()+1.5, n.getKey()+"");
                 }
                 
+    }
+
+    private void drawConnection(node_metadata src, node_metadata dest, Point2D label_location) {
+    
+        // find the right most point:
+        node_metadata right_node = null, left_node = null;
+
+        if (src.getLocation().x() > dest.getLocation().x()){
+            right_node = src;
+            left_node = dest;
+        }
+        else {
+            left_node = src;
+            right_node = dest;
+        }
+        Point2D p3 = new Point2D(right_node.getLocation().x(), left_node.getLocation().y());
+                
+        double distance_x = line.distance(right_node.getLocation(), left_node.getLocation());
+        double distance_y = right_node.getLocation().y() - p3.y();
+        double degree = (Math.asin(distance_y / distance_x)*180/Math.PI);
+        
+        System.out.println("distance_x=" + distance_x + "distance_y="+distance_y + "degree:" + (degree)  + "x=" + right_node.getLocation().x() + ",y=" + right_node.getLocation().y()  );
+        
+        
+        StdDraw.text(label_location.x(), label_location.y()+1.5, g.getEdge(src.getKey(), dest.getKey()).getTag(), degree);// print text connection
+       
+
     }
 }
---
+++ src/gui/NewClass.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import algorithms.line;
+import utils.Point2D;
+import utils.StdDraw;
+
+/**
+ *
+ * @author Yoni
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        StdDraw.setCanvasSize(400, 400);
+        StdDraw.setScale(0, 100);
+        StdDraw.setPenRadius(0.01);
+        
+        Point2D p1 = new Point2D(5, 95);
+        Point2D p2 = new Point2D(99, 3);
+        
+        Point2D p3 = line.getPointOnLine(p1, p2, 50);
+        StdDraw.line(p1.x(), p1.y(), p2.x(), p2.y());
+        
+        StdDraw.text(p3.x(), p3.y()+3, "shalom", -44.3839);
+        
+                
+    }
+}
