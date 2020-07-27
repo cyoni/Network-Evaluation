e0c52e32c25ca91ddd9c3475c82bb069057547c0
cyoni <44746539+cyoni@users.noreply.github.com>
2020-07-28 01:44:23 +0300

    Create Zone.java

---
+++ src/Graph/Algorithms/Zone.java
@@ -0,0 +1,19 @@
+
+package Graph.Algorithms;
+
+import Utils.Point2D;
+
+/**
+ *
+ * @author Yoni
+ */
+public class Zone {
+    Point2D position;
+    public int radius;
+    
+    public Zone(Point2D position, int radius){
+        this.position = position;
+        this.radius = radius;
+    }
+    
+}
