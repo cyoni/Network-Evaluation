cc3b5f36756952afa274c6e427c52774f56d2036
cyoni <44746539+cyoni@users.noreply.github.com>
2020-10-10 23:15:48 +0300

    Update Point2DTest.java

--- test/Utils/Point2DTest.java
+++ test/Utils/Point2DTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Utils;
 
 import org.junit.After;
@@ -12,10 +7,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- *
- * @author caron
- */
+
 public class Point2DTest {
 
     public Point2DTest() {
